0050222f2d5cedcde5ee7d72fd66c0b5be387605
Iana Fonteneles <ianafonteneles@gmail.com>
2023-01-26 13:25:38 -0300

    Tela de Perfil - Requisitos 57 à 62 (faltando os testes do 62)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+// import { Link } from 'react-router-dom';
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
 
-function Profile() {
-  return (
-    <div>
-      <Header title="Profile" />
-      <Footer />
-    </div>
-  );
-}
+// function Profile() {
+//   const readObject = (key, defaultValue) => {
+//     const object = localStorage.getItem(key);
+//     return JSON.parse(object) || defaultValue;
+//   };
+//   const USER_KEY = 'user';
+//   const getUser = () => readObject(USER_KEY, {});
+//   const userEmail = getUser().email;
 
-export default Profile;
+//   return (
+//     <div>
+//       <Header title="Profile" />
+//       <span data-testid="profile-email">{ userEmail }</span>
+//       <div>
+//         <Link to="/done-recipes">
+//           <button
+//             type="button"
+//             data-testid="profile-done-btn"
+//           >
+//             Done Recipes
+//           </button>
+//         </Link>
+
+//         <Link to="/favorite-recipes">
+//           <button
+//             type="button"
+//             data-testid="profile-favorite-btn"
+//           >
+//             Favorite Recipes
+//           </button>
+//         </Link>
+
+//         <Link to="/">
+//           <button
+//             type="button"
+//             data-testid="profile-logout-btn"
+//             onClick={ () => localStorage.clear() }
+//           >
+//             Logout
+//           </button>
+//         </Link>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default Profile;

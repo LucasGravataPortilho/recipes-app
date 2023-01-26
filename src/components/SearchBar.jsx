772b763488c24b706d252e8e32d63a1b54e0c1e7
daianne dorigatti <daiannedorigatti@gmail.com>
2023-01-26 17:37:56 -0300

    adicionando requisito 11

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,54 @@
+import React, { useState } from 'react';
+
 function SearchBar() {
+  const [inputSearch, setinputSearch] = useState('');
+
+  const fetchIngredientAPI = async () => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputSearch}`);
+    const result = await response.json();
+    console.log(result);
+    return result;
+  };
+  const fetchNameAPI = async () => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputSearch}`);
+    const result = await response.json();
+    console.log(result);
+    return result;
+  };
+  const fetchFirstLetterAPI = async () => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${inputSearch}`);
+    const result = await response.json();
+    console.log(result);
+    return result;
+  };
+
+  const handleChange = ({ target }) => {
+    setinputSearch(target.value);
+  };
+
+  const handleButtonSearch = () => {
+    if (document.getElementById('ingredient').checked) {
+      fetchIngredientAPI();
+    } else if (document.getElementById('name').checked) {
+      fetchNameAPI();
+    } else if (document.getElementById('first-letter').checked) {
+      fetchFirstLetterAPI();
+    }
+  };
+
   return (
+
     <>
       <div>
         <input
+          type="text"
+          placeholder="Search"
+          value={ inputSearch }
+          onChange={ handleChange }
+        />
+        <input
           type="radio"
+          id="ingredient"
           data-testid="ingredient-search-radio"
           name="button"
 
@@ -11,12 +56,14 @@ function SearchBar() {
         Ingredient
         <input
           type="radio"
+          id="name"
           data-testid="name-search-radio"
           name="button"
         />
         Name
         <input
           type="radio"
+          id="first-letter"
           data-testid="first-letter-search-radio"
           name="button"
         />
@@ -24,6 +71,8 @@ function SearchBar() {
       </div>
       <button
         data-testid="exec-search-btn"
+        type="button"
+        onClick={ handleButtonSearch }
       >
         Buscar
 

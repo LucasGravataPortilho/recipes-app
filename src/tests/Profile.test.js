1cb7e33ac4702d5386c2af9c0b2039526c48a927
Lucas Portilho <lucasportilho18@gmail.com>
2023-01-26 14:52:03 -0300

    requisito 62

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,35 +1,36 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// // import userEvent from '@testing-library/user-event';
-// import App from '../App';
-// import renderWithRouterAndRedux from './renderWith';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouterAndRedux from './renderWith';
+import Profile from '../pages/Profile';
 
-// describe('Testa se a página de Profile é exibida corretamente', () => {
-//   it('Testa se possui um header com o título "Profile"', () => {
-//     renderWithRouterAndRedux(<App />);
-//     const header = screen.getByRole('Profile');
+describe('Testa se a página de Profile é exibida corretamente', () => {
+  it('Testa se possui um header com o título "Profile"', () => {
+    renderWithRouterAndRedux(<Profile />);
+    const header = screen.getByText(/Profile/i);
 
-//     expect(header).toBeInTheDocument();
-//   });
-//   it('Testa se possui o elemento de email ', () => {
-//     renderWithRouterAndRedux(<App />);
-//     const email = screen.getByTestId('profile-email');
+    expect(header).toBeInTheDocument();
+  });
+  it('Testa se possui o elemento de email ', () => {
+    renderWithRouterAndRedux(<Profile />);
+    const email = screen.getByTestId('profile-email');
 
-//     expect(email).toBeInTheDocument();
-//   });
-//   it('Testa se possui um botão com o texto "Done Recipes"', () => {
-//     renderWithRouterAndRedux(<App />);
-//     const profileDoneBtn = screen.getById('profile-done-btn');
-//     expect(profileDoneBtn).toBeInTheDocument();
-//   });
-//   it('Testa se possui um botão com o texto "Favorite Recipes"', () => {
-//     renderWithRouterAndRedux(<App />);
-//     const profileFavBtn = screen.getById('profile-favorite-btn');
-//     expect(profileFavBtn).toBeInTheDocument();
-//   });
-//   it('Testa se possui um botão com o texto "Logout"', () => {
-//     renderWithRouterAndRedux(<App />);
-//     const profileLOBtn = screen.getById('profile-logout-btn');
-//     expect(profileLOBtn).toBeInTheDocument();
-//   });
-// });
+    expect(email).toBeInTheDocument();
+  });
+  it('Testa se possui um botão com o texto "Done Recipes"', () => {
+    renderWithRouterAndRedux(<Profile />);
+    const profileDoneBtn = screen.getByTestId('profile-done-btn');
+    expect(profileDoneBtn).toBeInTheDocument();
+  });
+  it('Testa se possui um botão com o texto "Favorite Recipes"', () => {
+    renderWithRouterAndRedux(<Profile />);
+    const profileFavBtn = screen.getByTestId('profile-favorite-btn');
+    expect(profileFavBtn).toBeInTheDocument();
+  });
+  it('Testa se possui um botão com o texto "Logout"', () => {
+    renderWithRouterAndRedux(<Profile />);
+    const profileLOBtn = screen.getByTestId('profile-logout-btn');
+    expect(profileLOBtn).toBeInTheDocument();
+    userEvent.click(profileLOBtn);
+  });
+});

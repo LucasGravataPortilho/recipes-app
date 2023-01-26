3e74cf2caf9599f3773b7a8ac07e01eb91b4f18b
Iana Fonteneles <ianafonteneles@gmail.com>
2023-01-26 13:34:12 -0300

    Tela de Perfil - Requisitos 57 à 62 (faltando os testes da 62)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,52 +1,52 @@
-// import { Link } from 'react-router-dom';
-// import Header from '../components/Header';
-// import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-// function Profile() {
-//   const readObject = (key, defaultValue) => {
-//     const object = localStorage.getItem(key);
-//     return JSON.parse(object) || defaultValue;
-//   };
-//   const USER_KEY = 'user';
-//   const getUser = () => readObject(USER_KEY, {});
-//   const userEmail = getUser().email;
+function Profile() {
+  const readObject = (key, defaultValue) => {
+    const object = localStorage.getItem(key);
+    return JSON.parse(object) || defaultValue;
+  };
+  const USER_KEY = 'user';
+  const getUser = () => readObject(USER_KEY, {});
+  const userEmail = getUser().email;
 
-//   return (
-//     <div>
-//       <Header title="Profile" />
-//       <span data-testid="profile-email">{ userEmail }</span>
-//       <div>
-//         <Link to="/done-recipes">
-//           <button
-//             type="button"
-//             data-testid="profile-done-btn"
-//           >
-//             Done Recipes
-//           </button>
-//         </Link>
+  return (
+    <div>
+      <Header title="Profile" />
+      <span data-testid="profile-email">{ userEmail }</span>
+      <div>
+        <Link to="/done-recipes">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Done Recipes
+          </button>
+        </Link>
 
-//         <Link to="/favorite-recipes">
-//           <button
-//             type="button"
-//             data-testid="profile-favorite-btn"
-//           >
-//             Favorite Recipes
-//           </button>
-//         </Link>
+        <Link to="/favorite-recipes">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+          </button>
+        </Link>
 
-//         <Link to="/">
-//           <button
-//             type="button"
-//             data-testid="profile-logout-btn"
-//             onClick={ () => localStorage.clear() }
-//           >
-//             Logout
-//           </button>
-//         </Link>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => localStorage.clear() }
+          >
+            Logout
+          </button>
+        </Link>
+      </div>
+      <Footer />
+    </div>
+  );
+}
 
-// export default Profile;
+export default Profile;

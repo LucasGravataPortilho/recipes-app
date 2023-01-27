a726eca5a98635b10ebe0942755d53930a656e3f
Pedro Machado <pedromportilho@gmail.com>
2023-01-26 23:47:17 -0300

    Correção do requisito 12

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { getDrinks } from '../services/api';
 
 function SearchBar() {
@@ -25,27 +25,28 @@ function SearchBar() {
 
   const handleChange = ({ target }) => {
     setinputSearch(target.value);
-    console.log(target.value.length);
   };
 
-  const handleButtonSearch = () => {
+  const handleButtonSearch = async () => {
     if (document.getElementById('ingredient').checked) {
-      fetchIngredientAPI();
-      getDrinks(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${inputSearch}`);
+      try {
+        fetchIngredientAPI();
+        await getDrinks(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${inputSearch}`);
+      } catch (error) {
+        global.alert('ingrediente não existe');
+      }
     } else if (document.getElementById('name').checked) {
       fetchNameAPI();
       getDrinks(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputSearch}`);
     } else if (document.getElementById('first-letter').checked) {
-      fetchFirstLetterAPI();
-      getDrinks(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${inputSearch}`);
+      if (inputSearch.length === 1) {
+        fetchFirstLetterAPI();
+        getDrinks(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${inputSearch}`);
+      } else {
+        global.alert('Your search must have only 1 (one) character');
+      }
     }
   };
-
-  useEffect(() => {
-    if (document.getElementById('first-letter').checked && inputSearch.length > 1) {
-      global.alert('Your search must have only 1 (one) character');
-    }
-  });
 
   return (
     <>
@@ -86,7 +87,6 @@ function SearchBar() {
         onClick={ handleButtonSearch }
       >
         Buscar
-
       </button>
     </>
   );

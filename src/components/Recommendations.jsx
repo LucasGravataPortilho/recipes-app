8e83d533f3ead66b03c272d69b4627eed194805d
Pedro Machado <pedromportilho@gmail.com>
2023-01-31 13:50:17 -0300

    Adicionando 26, 27 e 28

---
+++ src/components/Recommendations.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+
+function Recomendations({ type }) {
+  const [key, setKey] = useState('');
+  const [capital, setCapital] = useState('');
+  const [url, setUrl] = useState('');
+  const [recipe, setRecipe] = useState([]);
+
+  const requisicaoAPI = useCallback(async () => {
+    if (url !== '') {
+      try {
+        const numberItens = 6;
+        const response = await fetch(url);
+        const json = await response.json();
+        setRecipe(json[key].slice(0, numberItens));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, [url, key]);
+
+  const setVariables = useCallback(() => {
+    if (type === 'meals') {
+      setKey('drinks');
+      setCapital('Drink');
+      setUrl('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    } else if (type === 'drinks') {
+      setKey('meals');
+      setCapital('Meal');
+      setUrl('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    }
+  }, [type]);
+
+  useEffect(() => {
+    setVariables();
+    requisicaoAPI();
+  });
+
+  const recipeList = () => {
+    const items = recipe.map((e, i) => {
+      const item = (
+        <div key={ i } data-testid={ `${i}-recommendation-card` }>
+          <img
+            src={ e[`str${capital}Thumb`] }
+            alt="First slide"
+          />
+          <h3 data-testid={ `${i}-recommendation-title` }>{e[`str${capital}`]}</h3>
+        </div>
+      );
+      return item;
+    });
+
+    return items;
+  };
+
+  return (
+
+    <div>
+      <h2>Recomendations</h2>
+      <div
+        className="container"
+      >
+        {recipeList()}
+      </div>
+    </div>
+
+  );
+}
+
+Recomendations.propTypes = {
+  type: PropTypes.string,
+}.isRequired;
+
+export default Recomendations;

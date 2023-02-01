09048c69e3044c5f9b84dd624453932e66be7294
daianne dorigatti <daiannedorigatti@gmail.com>
2023-02-01 15:30:55 -0300

    adicionando requisito 32

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -63,6 +63,18 @@ function Recomendations({ type }) {
       >
         {recipeList()}
       </div>
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        Compartilhar
+      </button>
+      <button
+        type="button"
+        data-testid="favorite-btn"
+      >
+        Favoritar
+      </button>
     </div>
 
   );

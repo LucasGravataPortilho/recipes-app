34f6116435016fada798e12475d6265c7f2c11a7
Pedro Machado <pedromportilho@gmail.com>
2023-02-01 15:04:15 -0300

    Requisito 40

--- src/components/Checkboxes.jsx
+++ src/components/Checkboxes.jsx
@@ -1,23 +1,7 @@
 import { useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types'; // Sempre importante para declarar props
 
-function Checkboxes({ obj }) {
-  const [ingredients, setIngredients] = useState([]);
-
-  const getIngredients = useCallback(() => {
-    if (obj !== []) {
-      const ingredientValues = Object.values(obj).filter(
-        (e, i) => (Object.keys(obj)[i].includes('Ingredient')),
-      );
-      const semVazio = ingredientValues.filter((e) => (e !== null) && (e !== ''));
-      setIngredients(semVazio);
-    }
-  }, [obj]);
-
-  useEffect(() => {
-    getIngredients();
-  }, [getIngredients]);
-
+function Checkboxes({ obj, inProgress }) {
   function changeCheckbox({ target }) {
     const { value } = target;
 
@@ -30,39 +14,46 @@ function Checkboxes({ obj }) {
   }
 
   function createIngredients() {
-    const ings = ingredients.map((e, i) => {
-      if (e !== '' && e !== null) {
-        const ingredient = (
-          <label
-            data-testid={ `${i}-ingredient-step` }
-            key={ i }
-            htmlFor={ `ingredient-${i}` }
-            id={ i }
-            className=""
-          >
-            <input type="checkbox" onChange={ changeCheckbox } value={ i } />
-            <p>
-              {e}
-            </p>
-          </label>
-        );
-        return ingredient;
-      }
-      return ('');
-    });
+    if (obj !== []) {
+      const ingredientValues = Object.values(obj).filter(
+        (e, i) => (Object.keys(obj)[i].includes('Ingredient')),
+      );
+      const semVazio = ingredientValues.filter((e) => (e !== null) && (e !== ''));
 
-    return ings;
+      const ings = semVazio.map((e, i) => {
+        if (e !== '' && e !== null) {
+          const ingredient = (
+            <label
+              data-testid={ `${i}-ingredient-step` }
+              key={ i }
+              htmlFor={ `ingredient-${i}` }
+              id={ i }
+              className=""
+            >
+              <input type="checkbox" onChange={ changeCheckbox } value={ i } />
+              <p>
+                {e}
+              </p>
+            </label>
+          );
+          return ingredient;
+        }
+        return ('');
+      });
+      return ings;
+    }
   }
 
   return (
     <div>
-      {createIngredients()}
+      {(obj !== []) ? (createIngredients()) : ('')}
     </div>
   );
 }
 
 Checkboxes.propTypes = {
-  obj: PropTypes.shape().isRequired,
-};
+  obj: PropTypes.shape(),
+  inProgress: PropTypes.shape(),
+}.isRequired;
 
 export default Checkboxes;
--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -1,30 +1,17 @@
 import { useEffect, useState, useCallback } from 'react';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom';
-import Checkboxes from '../components/Checkboxes';
 import './RecipeinProgress.css';
 
 function RecipeInProgress() {
   const [key, setKey] = useState();
   const [capitalKey, setCapital] = useState();
   const [id, setId] = useState();
-  const [url, setUrl] = useState('');
   const [recipe, setRecipe] = useState({});
+  const [usedIngredients, setingredients] = useState([]);
+  const [lsAtual, setLS] = useState({});
   const location = useLocation();
 
-  const setVariables = useCallback(() => {
-    const path = location.pathname.split('/');
-    setKey(path[1]);
-    setId(path[2]);
-    setCapital(path[1].charAt(0).toUpperCase() + path[1].slice(1, path[1].length - 1));
-
-    if (key === 'meals') {
-      setUrl(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-    } else {
-      setUrl(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-    }
-  }, [id, key, location]);
-
-  const requisicaoAPI = useCallback(async () => {
+  const requisicaoAPI = useCallback(async (url) => {
     try {
       const response = await fetch(url);
       const json = await response.json();
@@ -32,12 +19,81 @@ function RecipeInProgress() {
     } catch (e) {
       console.log(e);
     }
-  }, [url, key]);
+  }, [key]);
 
   useEffect(() => {
-    setVariables();
-    requisicaoAPI();
-  }, [setVariables, requisicaoAPI]);
+    let url = '';
+    const path = location.pathname.split('/');
+    setKey(path[1]);
+    setId(path[2]);
+
+    if (path[1] === 'meals') {
+      url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${path[2]}`;
+      setCapital('Meal');
+    } else if (path[1] === 'drinks') {
+      url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${path[2]}`;
+      setCapital('Drink');
+    }
+
+    const lsOld = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    if (lsOld !== null) {
+      if (lsOld[path[1]][path[2]] === undefined) {
+        lsOld[path[1]][path[2]] = [];
+      }
+      setLS(lsOld);
+      setingredients(lsOld[path[1]][path[2]]);
+    } else {
+      const newLS = { drinks: {}, meals: {} };
+      newLS[path[1]][path[2]] = [];
+      setLS(newLS);
+      localStorage.setItem('inProgressRecipes', JSON.stringify(newLS));
+    }
+
+    requisicaoAPI(url);
+  }, [location, requisicaoAPI]);
+
+  function changeCheckbox({ target }) {
+    const { value } = target;
+    let lista = usedIngredients;
+
+    if (target.checked) {
+      lista.push(value);
+    } else {
+      lista = lista.filter((e) => e !== value);
+    }
+
+    lsAtual[key][id] = lista;
+    localStorage.setItem('inProgressRecipes', JSON.stringify(lsAtual));
+
+    setingredients(lista);
+  }
+
+  function createIngredients() {
+    const ingredientValues = Object.values(recipe).filter(
+      (e, i) => (Object.keys(recipe)[i].includes('Ingredient')),
+    );
+    const semVazio = ingredientValues.filter((e) => (e !== null) && (e !== ''));
+
+    const ingredients = semVazio.map((e, i) => {
+      const ingredient = (
+        <label
+          data-testid={ `${i}-ingredient-step` }
+          key={ i }
+          htmlFor={ `ingredient-${i}` }
+        >
+          <input
+            type="checkbox"
+            onChange={ changeCheckbox }
+            value={ e }
+            defaultChecked={ usedIngredients.includes(e) }
+          />
+          {e}
+        </label>
+      );
+      return ingredient;
+    });
+    return ingredients;
+  }
 
   return (
     <div>
@@ -48,10 +104,28 @@ function RecipeInProgress() {
         src={ recipe[`str${capitalKey}Thumb`] }
       />
       <h1 data-testid="recipe-title">{recipe[`str${capitalKey}`]}</h1>
-      <button type="button" data-testid="share-btn">Share</button>
+      <button
+        type="button"
+        onClick={ () => {
+          console.log(key);
+          console.log(capitalKey);
+          console.log(id);
+          console.log(recipe);
+          console.log(lsAtual);
+          console.log(usedIngredients);
+        } }
+      >
+        Teste
+      </button>
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        Share
+      </button>
       <button type="button" data-testid="favorite-btn">Favorite</button>
       <h3 data-testid="recipe-category">{recipe.strCategory}</h3>
-      <Checkboxes obj={ recipe } />
+      {(recipe !== {}) ? (createIngredients()) : ('')}
       <p data-testid="instructions">{recipe.strInstructions}</p>
       <button type="button" data-testid="finish-recipe-btn">Finish</button>
     </div>

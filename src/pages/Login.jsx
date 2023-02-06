ac6185153055e7cf5373e12bfb11e070aa832449
Vitória <vitcs11@gmail.com>
2023-02-06 13:01:02 -0300

    Estilização da tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Login.css';
+import shawarma from '../images/Shawarma-pana.png';
 
 class Login extends React.Component {
   state = {
@@ -35,42 +36,50 @@ class Login extends React.Component {
 
   render() {
     const { email, password, isLoginButtonDisabled } = this.state;
-
     return (
-      <div className="form-login">
-        <form>
-          <h2>Sign in</h2>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={ email }
-            placeholder="Digite seu email"
-            data-testid="email-input"
-            onChange={ this.handleChange }
-            className="email-input"
-          />
+      <div className="login-page">
+        <div className="first-box">
+          <form>
+            <h1>Trybe Kitchen</h1>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={ email }
+              placeholder="Digite seu email"
+              data-testid="email-input"
+              onChange={ this.handleChange }
+              className="email-input"
+            />
 
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={ password }
-            placeholder="Digite sua senha"
-            data-testid="password-input"
-            onChange={ this.handleChange }
-            className="password-input"
-          />
-          <button
-            data-testid="login-submit-btn"
-            type="submit"
-            disabled={ isLoginButtonDisabled }
-            onClick={ this.handleClickButton }
-            className="button-login"
-          >
-            Entrar
-          </button>
-        </form>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={ password }
+              placeholder="Digite sua senha"
+              data-testid="password-input"
+              onChange={ this.handleChange }
+              className="password-input"
+            />
+            <button
+              data-testid="login-submit-btn"
+              type="submit"
+              disabled={ isLoginButtonDisabled }
+              onClick={ this.handleClickButton }
+              className="button-login"
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
+        <div className="second-box">
+          <b>
+            &quot;Mais tempo para desfrutar, menos tempo para planejar suas
+            refeições.&quot;
+          </b>
+          <img src={ shawarma } alt="shawarma" className="second-box-image" />
+        </div>
       </div>
     );
   }
